cd77765704fff022b7381e2edf45bd21da719664
Pratik Shinde <pratikshinde9957@gmail.com>
2026-02-01 23:18:21 +0530

    cors update

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
     cors({
-         origin: "http://localhost:3000",
+         origin: ["http://localhost:3000", "https://hireflow-ai-eight.vercel.app"],
          credentials: true,
     })
 );
