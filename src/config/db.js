1391ec2aeb7fbdc0f32b18857cd20638e63c7e80
Pratik Shinde <pratikshinde9957@gmail.com>
2026-02-02 00:32:08 +0530

    UPDATED DB STRING

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,15 @@
-const {Sequelize} = require('sequelize');
-require('dotenv').config();
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'postgres',
-    }
-);
-module.exports = {sequelize};
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  logging: false,
+});
+
+module.exports = { sequelize };
